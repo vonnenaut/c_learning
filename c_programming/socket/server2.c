b02850f9ac61468dd64986d0bd2b2bb61bd9928a
vonnenaut <danielashcom@gmail.com>
2021-01-04 20:26:31 +0700

    automated sync commit for c_learning

---
+++ c_programming/socket/server2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netinet/in.h>
+#include <unistd.h>
+
+int main() {
+    char server_message[256] = "You have reached the server!";
+
+    // create server socket
+    int server_socket;
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // define server address
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    // bind the socket to our specified IP and port
+    bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+
+    // listen for connections
+    listen(server_socket, 5);
+
+    // accept connections from client sockets
+    int client_socket;
+    client_socket = accept(server_socket, NULL, NULL);
+
+    // send data (message) to client via client socket
+    send(client_socket, server_message, sizeof(server_message), 0);
+
+    // close the socket
+    close(server_socket);
+
+    return 0;
+}
