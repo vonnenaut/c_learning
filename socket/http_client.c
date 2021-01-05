9a7281ab2c2676a6415feca238f057b9cf410c84
vonnenaut <danielashcom@gmail.com>
2021-01-05 21:00:29 +0700

    automated sync commit for c_learning

--- socket/http_client.c
+++ socket/http_client.c
@@ -7,9 +7,12 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int main(int argc, char *argv) {
+#include <unistd.h>
+
+int main(int argc, char *argv)
+{
     char *address;
-    address = argv[1];
+    address = &argv[1];
 
     int client_socket;
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -18,6 +21,7 @@ int main(int argc, char *argv) {
     struct sockaddr_in remote_address;
     remote_address.sin_family = AF_INET;
     remote_address.sin_port = htons(80);
+
     inet_aton(address, &remote_address.sin_addr.s_addr);
 
     connect(client_socket, (struct sockaddr *) &remote_address, sizeof(remote_address));
@@ -26,7 +30,10 @@ int main(int argc, char *argv) {
     char response[4096];
 
     send(client_socket, request, sizeof(request), 0);
-    receive(client_socket, &response, sizeof(response), 0);
+    recv(client_socket, &response, sizeof(response), 0);
+
+    printf("response from the server: %s\n", response);
+    close(client_socket);
 
     return 0;
 }
